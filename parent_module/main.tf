856c8f8e36175e6bf96eb2f5cdb1c0e49fdbd833
Digambar-9438 <digambar.naya6080@gmail.com>
2025-09-11 01:31:32 +0530

    add now

--- parent_module/main.tf
+++ parent_module/main.tf
@@ -1,14 +1,14 @@
 module "md_resource_group" {
   source              = "../module/azurerm_resourc_group" # fixed spelling of module folder
-  resource_group_name = "rg-vivekb123"
+  resource_group_name = "rg-vivekb1243"
   location            = "west us"
 }
 
 module "md_stroage_account" {
   depends_on           = [module.md_resource_group]
   source               = "../module/azurerm_stroage_account"
-  storage_account_name = "vivekstroageacct9876"
-  resource_group_name  = "rg-vivekb123"
+  storage_account_name = "vivekstroageacct986"
+  resource_group_name  = "rg-vivekb1243"
   location             = "west us"
  }
 
