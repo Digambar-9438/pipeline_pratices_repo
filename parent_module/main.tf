fa4f8ae84e93b5b2d6b0315f7e59da7502e7a009
Digambar-9438 <digambar.naya6080@gmail.com>
2025-09-11 01:14:34 +0530

    add ;

--- parent_module/main.tf
+++ parent_module/main.tf
@@ -7,12 +7,12 @@ module "md_resource_group" {
 module "md_stroage_account" {
   depends_on           = [module.md_resource_group]
   source               = "../module/azurerm_stroage_account"
-  storage_account_name = "vivekstroageaccount143"
+  storage_account_name = "vivekbaliyanstroageaccount1"
   resource_group_name  = "rg-vivekb123"
   location             = "west us"
  }
 
- 
+
 # Creates an Azure Storage Account with security tool integration.
 # Security Tool: Please specify the name of the security tool being used for monitoring or compliance.
 # This resource ensures secure storage for your data and can be configured to meet organizational security requirements.
